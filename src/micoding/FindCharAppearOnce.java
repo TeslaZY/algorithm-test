ccda9d9539b7ad54f4482ad284c30436d755326d
TeslaZY <18834827690@163.com>
2018-09-05 16:21:12 +0800

    描述 给出N个数字。其中仅有一个数字出现过一次，其他数字均出现过两次，找出这个出现且只出现过一次的数字。要求时间和空间复杂度最小。
    输入
    输入多个数字，每个数字以空格分开，回车结束
    
    输出
    输出内容为只出现过唯一一次的数字
    
    输入样例
    10 10 11 12 12 11 16
    
    输出样例
    16

---
+++ src/micoding/FindCharAppearOnce.java
@@ -0,0 +1,19 @@
+package micoding;
+
+public class FindCharAppearOnce extends  SuperCodeClass{
+    @Override
+    public String solution(String line){
+        String[] array= line.split(" ");
+        int result=0;
+        result=Integer.valueOf(array[0])^Integer.valueOf(array[1]);
+        for(int i=2;i<array.length;i++){
+            result=result^Integer.valueOf(array[i]);
+        }
+        return Integer.toString(result);
+    }
+
+    public static void main(String[] args) {
+        String line = input();
+        System.out.println(new BigNumberSub().solution(line));
+    }
+}
