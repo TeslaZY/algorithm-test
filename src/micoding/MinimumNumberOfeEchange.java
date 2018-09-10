1d1b2048d8c49a0f04f28df1521984847b73f0c6
TeslaZY <18834827690@163.com>
2018-09-10 17:27:54 +0800

    求把乱序数列变为升序。元素需要的最小交换次数

--- src/micoding/MinimumNumberOfeEchange.java
+++ src/micoding/MinimumNumberOfeEchange.java
@@ -4,20 +4,57 @@ import java.util.Scanner;
 
 /**
  * @author TeslaZY
+ * 还是模拟原始做法：
+ *
+ * 4 8 2 7 5 6 1 3
+ * 1 4 8 2 7 5 6 3------>6次
+ *
+ * 1 2 4 8 7 5 6 3------>2次
+ *
+ * 1 2 3 4 8 7 5 6------>5次
+ *
+ * 1 2 3 4 5 8 7 6------>2次
+ *
+ * 1 2 3 4 5 6 8 7------>2次
+ *
+ * 1 2 3 4 5 6 7 8------>1次
  */
 public class MinimumNumberOfeEchange extends SuperCodeClass{
     @Override
-    public String solution(String line){
-        String[] arrays = line.split(",");
-        int i=0,j=0,count =0;
-        String temp;
-        for(i=1;i<arrays.length;i++){
-            for(j=i;j>0&&Integer.valueOf(arrays[j]) < Integer.valueOf(arrays[j - 1]);j--) {
-                    temp = arrays[i];
-                    arrays[i] = arrays[i - 1];
-                    arrays[i - 1] = temp;
-                    count++;
+    public String solution(String line) {
+        String[] str_arrays = line.split(",");
+        int arrays[] = new int[str_arrays.length];
+        int i = 0, j = 0, count = 0;
+        for (i = 0; i < str_arrays.length; i++) {
+            arrays[i] = Integer.valueOf(str_arrays[i]);
+        }
+        boolean finish = true;
+        while (true) {
+            int index = -1;
+            int min_value = Integer.MAX_VALUE;
+            for (i = 0; i < arrays.length; i++) {
+                if (arrays[i] != -1 && arrays[i] < min_value) {
+                    min_value = arrays[i];
+                    index = i;
+                }
             }
+            for (j = 0; j < index; j++) {
+                if (arrays[j] != -1 && arrays[j] > min_value) {
+                    count++;
+                }
+            }
+            arrays[index] = -1;
+            for (j = 0; j < arrays.length; j++) {
+                if (arrays[j] != -1) {
+                    finish = false;
+                    break;
+                }
+            }
+
+            if (finish) {
+                break;
+            }
+            finish = true;
         }
         return Integer.toString(count);
     }
