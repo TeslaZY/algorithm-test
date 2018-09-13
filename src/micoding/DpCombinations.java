50ddf314dac014466d891a036f3c3a7b376b5616
TeslaZY <18834827690@163.com>
2018-09-13 14:39:13 +0800

    动态规划  用arr数组元素拼凑 aim

--- src/micoding/DpCombinations.java
+++ src/micoding/DpCombinations.java
@@ -36,7 +36,26 @@ public class DpCombinations {
         // 返回dp矩阵最右下角的值即为最后结果
         return Integer.toString(dp[arr.length - 1][aim]);
     }
-
+/*
+    public static int process4(int[] arr, int aim) {
+        // 创建dp矩阵
+        int[][] dp = new int[arr.length][aim + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 1; // 凑成0元的方法必然是什么货币都不用，只有1种
+            for (int j = 1; j < dp[i].length; j++) {
+                if (i == 0) {
+                    dp[i][j] = j % arr[i] == 0 ? 1 : 0;
+                } else if(j >= arr[i]){
+                    dp[i][j] = dp[i][j - arr[i]] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        // 返回dp矩阵最右下角的值即为最后结果
+        return dp[arr.length - 1][aim];
+    }
+ */
  /*   public static int process(int[] arr,int aim) {
         int[][] dp = new int[arr.length][aim + 1];
         for (int i = 0; i < dp.length; i++) {
