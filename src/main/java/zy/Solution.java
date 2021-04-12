9b8a4ce46c6cd26783e86e9056d45edb1ac09f87
Tesla.Z <Tesla.Z@fanruan.com>
2021-04-12 11:59:19 +0800

    生成最大数

--- src/main/java/zy/Solution.java
+++ src/main/java/zy/Solution.java
@@ -1,19 +1,17 @@
 package zy;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @author Tesla.Z
- * @version 10.0
- * Created by Tesla.Z on 2020/9/2
+ * @Author: Tesla.Z
+ * @Date: 2021/4/12 11:00
+ * @Description
  */
-
-public class BiggestNumber {
+class Solution {
 
     private static String ZERO = "0";
 
@@ -40,15 +38,16 @@ public class BiggestNumber {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader buf=new BufferedReader(new InputStreamReader(System.in));
-        String str = buf.readLine();
-        int n = Integer.valueOf(str);
-        String[] array = new String[n];
-        for (int i = 0; i < n; i++) {
-            array[i] = buf.readLine();
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int numArray[][] = {
+                {0,0,0,0},
+                {1,33,0,4},
+                {2,4,56,78,233,456,8},
+                {1,2,3,4,5,6,7,8,9}
+        };
+        for(int[] nums : numArray ) {
+            System.out.println(solution.largestNumber(nums));
         }
-        String result = Arrays.stream(array).parallel().sorted((String s1, String s2) -> (s2 + s1).compareTo(s1 + s2)).collect(Collectors.joining());
-        System.out.println(result);
     }
 }
