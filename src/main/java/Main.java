b97aa7203ebe62b7d0af20156f34b97c60ad44e6
Tesla.Z <Tesla.Z@fanruan.com>
2021-04-27 20:05:57 +0800

    MinimumSpanningTree

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class Main {
+
+
+}
---
+++ src/main/java/zy/MinimumSpanningTree.java
@@ -0,0 +1,21 @@
+package zy;
+
+import java.io.*;
+
+/**
+ * @Author: Tesla.Z
+ * @Date: 2021/4/27 20:01
+ * @Description
+ */
+public class MinimumSpanningTree {
+    public static void main(String[] args){
+        try {
+            ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(System.in));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+            int n = in.readInt();
+            int m = in.readInt();
+        }catch (Exception e){
+
+        }
+    }
+}
